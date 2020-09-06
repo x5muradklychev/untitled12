de84988accc67c0a813b584e58757955e5023194
murad.klychev <murad.klychev@x5.ru>
2020-09-07 00:02:03 +0300

    задача 7

--- src/Program7.java
+++ src/Program7.java
@@ -12,20 +12,21 @@ public class Program7 {
         int k = 491;
         int c = 800;
 
-       switch (m) {
+       switch (kod) {
            case 905:
-                System.out.println("Москва. Стоимость разговора: 4.15");
+                System.out.println("Москва. Стоимость разговора:" + 10 * 4.15);
            break;
            case 194:
-                System.out.println("Ростов. Стоимость разговора: 1.98");
+                System.out.println("Ростов. Стоимость разговора:" +  10 *  1.98);
                 break;
            case 491:
-               System.out.println("Краснодар. Стоимость разговора: 2.69");
+               System.out.println("Краснодар. Стоимость разговора:" +  10 * 2.69);
                break;
            case 800:
-                   System.out.println("Киров. Стоимость разговора: 5.00");
+                   System.out.println("Киров. Стоимость разговора:" +  10 *  5.00);
                    break;
 
         }
+        input.close();
     }
 }
