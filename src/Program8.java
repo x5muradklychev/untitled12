f5f552acc6c0829a7007e26616d4880f6d0af2d6
murad.klychev <murad.klychev@x5.ru>
2020-09-07 16:18:56 +0300

    задача 8 дописал код по новому

--- src/Program8.java
+++ src/Program8.java
@@ -1,3 +1,4 @@
+/*
 import java.util.*;
 
 public class Program8 {
@@ -42,6 +43,51 @@ public class Program8 {
             }
         }
         mas = ssum / age.length;
-        System.out.println("Cреднее арифметическое массива:" + ssum);
+        System.out.println("Cреднее арифметическое отрицательных элементов:" + ssum / mas );
+    }
+}
+*/
+public class Program8 {
+    public static void main(String[] args) {
+
+        int[] numbers = { 1, -10, 5, 6, 45, 23, -45, -34, 0, 32, 56, -1, 2, -2 };
+
+        int i;
+
+        int max = numbers[0];
+        int sumPos = 0;
+        int sumNegEven = 0;
+        int sumNeg = 0;
+        int countPosElem = 0;
+        double avgNeg = 0;
+
+        for (i = 0; i < numbers.length; i++) {
+            // максимальное значение
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+
+            if (numbers[i] >= 0) {
+                // - сумма положительных элементов
+                sumPos += numbers[i];
+                // - количество положительных элементов
+                countPosElem++;
+            } else {
+                // - сумма четных отрицательных элементов
+                if (numbers[i] % 2 == 0) {
+                    sumNegEven += numbers[i];
+                }
+                sumNeg += numbers[i];
+            }
+        }
+
+        // среднее арифметическое отрицательных элементов
+        avgNeg = (double) sumNeg / (numbers.length - countPosElem);
+
+        System.out.println("Максимальное значение массива: " + max);
+        System.out.println("Cумма положительных элементов массива: " + sumPos);
+        System.out.println("Количество положительных элементов: " + countPosElem);
+        System.out.println("Cумма четных отрицательных элементов: " + sumNegEven);
+        System.out.printf("Cреднее арифметическое отрицательных элементов: %.2f", avgNeg);
     }
 }
